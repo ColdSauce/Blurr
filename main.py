4aa85ffb363a57638c8d89abf7a74dd0f5c8ef57
Stefan <soccerfanatic1996@gmail.com>
2014-11-24 18:17:10 -0500

    fixed width height

--- main.py
+++ main.py
@@ -6,36 +6,33 @@ def getPixels(path):
 	im = Image.open(path).convert("RGB")
 	pixels = list(im.getdata())
 	width, height = im.size
-	pixels = [pixels [i * width:(i + 1) * width] for i in xrange(height)]
+
+	pixels = [pixels [i * width:(i + 1) * width] for i in range(height)]
 	return (pixels,width,height)
 
 
 def main():
 	pixels,width,height = getPixels("example.jpg")
 	pygame.init()
-	screen = pygame.display.set_mode((2001,2001))
+	screen = pygame.display.set_mode((1334,2000))
 	pygame.display.set_caption('Blurr')
 
 	background = pygame.Surface(screen.get_size())
 	background = background.convert()
 
 	screen.blit(background,(0,0))
-	for x in range(height):
-		for y in range(width):
-			try:
-				backgroundx.set_at((x,y),pixels[x][y])
-			except:
-				print str(x) + " , " + str(y)
+	for x in range(width)  :
+		for y in range(height):
+			background.set_at((x,y),pixels[x][y])
 
-	pygame.display.flip()
+
+
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				return
 		screen.blit(background,(0,0))
-		pygame.display.flip()
-
 
 
 if __name__ == "__main__":
